aed2c79fc33ecbbe8f26e6e960f16b8e3e835d64
rudrakabir <rudrakabir@gmail.com>
2023-09-21 02:27:06 +0530

    tail

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./src/**/*.{astro,js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
